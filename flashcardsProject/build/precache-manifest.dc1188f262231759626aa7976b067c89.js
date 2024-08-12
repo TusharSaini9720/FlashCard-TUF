be574dc968fe9921e457be212a5e04280f1d6a97
TusharSaini9720 <itsaini9720@gmail.com>
2024-08-12 23:31:09 +0530

    initail commit

---
+++ flashcardsProject/build/precache-manifest.dc1188f262231759626aa7976b067c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51fe57aa5e662249fa76da5607c0b40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00e5d0ac2b2faae0c542",
+    "url": "/static/css/main.103096e3.chunk.css"
+  },
+  {
+    "revision": "b42f0407f25c06567e68",
+    "url": "/static/js/2.4f0dd257.chunk.js"
+  },
+  {
+    "revision": "1876019e6f8cf7ca83e2",
+    "url": "/static/js/3.0e4cdb98.chunk.js"
+  },
+  {
+    "revision": "00e5d0ac2b2faae0c542",
+    "url": "/static/js/main.36e88743.chunk.js"
+  },
+  {
+    "revision": "08358a26ce333fe80872",
+    "url": "/static/js/runtime~main.b8f3c4c4.js"
+  }
+]);
