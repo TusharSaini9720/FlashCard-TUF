9b8414d392213ebe73a0a71e0dd4971757e05e1c
TusharSaini9720 <itsaini9720@gmail.com>
2024-08-13 07:38:07 +0530

    path

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ db.connect(err => {
 app.use(express.static(path.join(__dirname, '../flashcardsProject/build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'flashcardsProject','build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../flashcardsProject/build', 'index.html'));
 });
 
 app.get('/flashcards', (req, res) => {
